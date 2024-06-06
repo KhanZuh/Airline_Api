10cc06d26103be0abd8e459fc992a0a48d5a91f1
KhanZuh <khanzuhair96@live.co.uk>
2024-06-06 08:28:33 +0100

    Passenger MVP done, except booking a passenger onto a flight

--- start_point/src/main/java/com/example/airline_api/services/PassengerService.java
+++ start_point/src/main/java/com/example/airline_api/services/PassengerService.java
@@ -1,9 +1,13 @@
 package com.example.airline_api.services;
 
 
+import com.example.airline_api.models.Passenger;
 import com.example.airline_api.repositories.PassengerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PassengerService {
@@ -12,7 +16,17 @@ public class PassengerService {
     PassengerRepository passengerRepository;
 
 
+    public Passenger addNewPassenger(Passenger passenger) {
+        return passengerRepository.save(passenger);
+    }
 
+    public List<Passenger> getAllPassengers() {
+        return passengerRepository.findAll();
+    }
+
+    public Optional<Passenger> getPassengerById(Long id) {
+        return passengerRepository.findById(id);
+    }
 
 
 
